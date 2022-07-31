974e7b82e8ca95b4ba7ae2ad803f93d5ae4e5274
createhb21 <alsghk9701@gmail.com>
2022-07-31 21:37:11 +0900

    font update

---
+++ src/components/SiteMap/sitemap-generator.js
@@ -0,0 +1,9 @@
+require('babel-register')({ presets: ['es2015', 'react'] });
+const router = require('./sitemap-routes').default;
+const Sitemap = require('react-router-sitemap').default;
+function generateSitemap() {
+  return new Sitemap(router)
+    .build('https://www.everypd.co.kr/')
+    .save('./public/sitemap.xml');
+}
+generateSitemap();
--- src/styles/global.js
+++ src/styles/global.js
@@ -5,7 +5,7 @@ const GlobalStyle = createGlobalStyle`
   ${reset};
   *, *::before, *::after {
     box-sizing: border-box;
-    font-family: 'Noto Sans KR', sans-serif; 
+    font-family: 'Pretendard', 'Pretendard Variable', 'Noto Sans KR', sans-serif; 
   }
   html, body {
     overflow-x: hidden;
@@ -13,7 +13,7 @@ const GlobalStyle = createGlobalStyle`
   body {
     position: relative;
 
-    font-family: 'Noto Sans KR', -apple-system, 'Apple SD Gothic Neo', 'Noto Sans CJK KR', BlinkMacSystemFont, 'Helvetica Neue', 'Malgun Gothic', '맑은 고딕', 'Nanum Gothic', arial, 돋움, Dotum, Tahoma, Geneva, sans-serif;
+    font-family: 'Pretendard', 'Pretendard', 'Noto Sans KR', -apple-system, 'Apple SD Gothic Neo', 'Noto Sans CJK KR', BlinkMacSystemFont, 'Helvetica Neue', 'Malgun Gothic', '맑은 고딕', 'Nanum Gothic', arial, 돋움, Dotum, Tahoma, Geneva, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
